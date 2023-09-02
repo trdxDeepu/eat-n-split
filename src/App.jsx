a6204c659fecdec02e462a22a7dcddcb123683dc
trdxlucifer <singhdepu566@gmail.com>
2023-09-02 15:36:46 +0530

    added state in form

--- src/App.jsx
+++ src/App.jsx
@@ -90,12 +90,23 @@ function Friend({ friend, key }) {
 }
 
 function FormAddFriend() {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+
   return (
     <form className="form-add-friend">
       <label> 👨‍👩‍👧‍👦 Friend Name</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
       <label>📷Image URL</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={image}
+        onChange={(e) => setImage(e.target.value)}
+      />
       <Button>Add Friend</Button>
     </form>
   );
